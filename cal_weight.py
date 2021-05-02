a7ce45548ec23e3f48e8ba3d117b9d0c940a7716
designC <znehraks@gmail.com>
2021-05-03 02:27:41 +0900

    c

--- cal_weight.py
+++ cal_weight.py
@@ -13,6 +13,7 @@ import time
 import io
 import sys
 from operator import itemgetter
+from haversine import haversine
 
 # 먼저 학교 이름으로 학교의 중심 위도,경도를 알아오기
 
@@ -104,24 +105,37 @@ def get_residence_address(address_lon, address_lat):
 # 학교 좌표와 해당 매물과의 거리를 모두 구하여(맨하탄거리) 1위부터 꼴찌까지 가중치를 구함(이전에, Q2의 최대거리에서 필터링하여 해당 거리를 벗어나는 좌표에 있는 항목은 이 단계에서 걸러냄)
 
 
-def cal_T1(refined_residence, univ_lon, univ_lat):
-    # 맨하탄거리의 합으로 이루어진 배열 생성 후 저장
-    taxicab_geom = []
+def cal_T1(refined_residence, univ_lon, univ_lat, limit_dist):
+    limit_dist = float(limit_dist)
+    # 직선거리로 이루어진 배열 생성 후 저장
+    loc = []
+    # abs(i["lat"]-univ_lat)+abs(i["lon"]-univ_lon)
     for i in refined_residence:
-        taxicab_geom.append({"lgeo": i["lgeo"], "dist":
-                             abs(i["lat"]-univ_lat)+abs(i["lon"]-univ_lon), "lat": i["lat"], "lon": i["lon"]})
-    # 가까운 순으로 정렬
-    sorted_taxicab_geom = sorted(
-        taxicab_geom, key=itemgetter('dist'))
+        loc.append({"lgeo": i["lgeo"], "dist": haversine(
+            (i["lat"], i["lon"]), (univ_lat, univ_lon), unit='m'), "lat": i["lat"], "lon": i["lon"],
+            "count": i["count"]})
 
-    # 맨하탄거리로 서열을 매겨 총 개수로 나눈 후 100점 만점(1위) 부터 0점까지(꼴찌) 1위부터 높은 점수로 배열함
+    # 가까운 순으로 정렬
+    sorted_loc = sorted(
+        loc, key=itemgetter('dist'))
+
+    # limit_dist를 벗어나는 매물 제거
+    sorted_loc_filtered = []
+    for index, i in enumerate(sorted_loc):
+        if sorted_loc[index]["dist"] <= limit_dist:
+            sorted_loc_filtered.append(i)
+        else:
+            break
+
+    # 직선거리로 서열을 매겨 총 개수로 나눈 후 100점 만점(1위) 부터 0점까지(꼴찌) 1위부터 높은 점수로 배열함
     # ex.총 매물이 50일 경우, 1위가 100점, 2위가 98점, 3위가 96점 ... 50위가 0점 (동률에 대한 고려는 추후에 함)
     res = []
-    len_sorted_taxicab_geom = len(sorted_taxicab_geom)
+    len_sorted_loc_filtered = len(sorted_loc_filtered)
     # print(len_sorted_taxicab_geom)
-    for index, i in enumerate(sorted_taxicab_geom):
-        res.append({"lgeo": i["lgeo"], "dist": i["dist"], "lat": i["lat"], "lon": i["lon"],
-                    "T1_weight": 100*(1/len_sorted_taxicab_geom)*(len_sorted_taxicab_geom-index)})
+    for index, i in enumerate(sorted_loc_filtered):
+        res.append({"lgeo": i["lgeo"],
+                    "count": i["count"], "dist": i["dist"], "lat": i["lat"], "lon": i["lon"],
+                    "T1_weight": 100*(1/len_sorted_loc_filtered)*(len_sorted_loc_filtered-index)})
     return res
 
 # 각 매물에서 해당 구에 속한 지하철역중 가장 가까운 것의 거리를 가중치로 매겨서 그것 역시 1위부터 꼴찌까지 가중치로 매김
@@ -337,16 +351,15 @@ w5 = sys.argv[7]
 
 (univ_lon, univ_lat) = get_address_json(address_json)
 refined_residence = get_residence_address(univ_lon, univ_lat)
-T1 = cal_T1(refined_residence, univ_lon, univ_lat)
-T2 = cal_T2(refined_residence)
-T3 = cal_T3(refined_residence)
-T4 = cal_T4(refined_residence)
-T5 = cal_T5(refined_residence)
+T1 = cal_T1(refined_residence, univ_lon, univ_lat, limit_dist)
+T2 = cal_T2(T1)
+T3 = cal_T3(T1)
+T4 = cal_T4(T1)
+T5 = cal_T5(T1)
 total = get_final_weight(T1, T2, T3, T4, T5, w1, w2, w3, w4, w5)
 
 total = json.dumps(total)
 print(total)
-# print(total)
 # print(limit_dist)
 # print(weightcode)
 
@@ -357,6 +370,9 @@ print(total)
 # print(len(T3))
 # print(len(T4))
 # print(len(T5))
+# for i in refined_residence:
+#     print(i)
+
 # for i in T1:
 #     print(i)
 
